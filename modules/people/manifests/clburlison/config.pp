2e01efc49be408f70dd5b2a7cfae6ee77c6344ff
Clayton Burlison <clburlison@gmail.com>
2014-04-11 14:59:58 -0500

    test license vmware fusion

--- modules/people/manifests/clburlison/config.pp
+++ modules/people/manifests/clburlison/config.pp
@@ -19,4 +19,17 @@ class people::clburlison::config {
 	  $recovery_message = "Property of Birdville ISD, please contact Clayton Burlison if found. clayton.burlison@birdvilleschools.net or 817-547-3819."
 	  osx::recovery_message { $recovery_message: }
   }
+  
+  ###########################
+  # License VMWare Fusion 6 #
+  ###########################
+  
+  file { "/Users/${::luser}/Dropbox/Config/Preferences/System/Library/VMWare Fusion/license-fusion-60-e3-201303":
+	ensure  => link,
+    force   => true,
+	target  => "/Library/VMWare Fusion/license-fusion-60-e3-201303",
+    owner   => root,
+    mode    => 0644,
+	require => File["/Applications/VMWare Fusion.app/"]
+  } 
 }
