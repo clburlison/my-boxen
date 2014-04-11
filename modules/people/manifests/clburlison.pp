9f68600b689d3b7c78943b0dc46374916571ed76
Clayton Burlison <clburlison@gmail.com>
2014-04-10 20:42:26 -0500

    change clburlison.pp to be more simple. maybe this will work.

--- modules/people/manifests/clburlison.pp
+++ modules/people/manifests/clburlison.pp
@@ -1,8 +1,3 @@
-class people::clburlison (
-  $my_username  = $people::clburlison::params::my_username,
-  $my_homedir   = $people::clburlison::params::my_homedir,
-  $my_sourcedir = $people::clburlison::params::my_sourcedir,
-) inherits people::clburlison::params {
-  ## Declare all subclasses
+class people::clburlison{
   include people::clburlison::applications
 }
