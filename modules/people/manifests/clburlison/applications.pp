2c779bd5b8e8b5d255f976b290bc454c9eae7d93
Clayton Burlison <clburlison@gmail.com>
2014-04-11 13:27:24 -0500

    change remote desktop provider type for ccc, remote desktop, & add 7zX. Standardize all providers to be under the source with single quotes.

--- modules/people/manifests/clburlison/applications.pp
+++ modules/people/manifests/clburlison/applications.pp
@@ -17,9 +17,14 @@ class people::clburlison::applications {
   include vlc
   include vmware_fusion
   
+  package {'7zX':
+	source   => '/Users/${::luser}/Dropbox/Config/Software/7xX/7zX_1.7.1.dmg',
+	provider => 'appdmg',
+  }
+  
   package {'Adobe Flash':
-	provider => 'pkgdmg',
 	source   => 'http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_12_osx_pkg.dmg',
+	provider => 'pkgdmg',
   }
   
 	#   package {'Android File Transfer':
@@ -29,52 +34,52 @@ class people::clburlison::applications {
   
   package { 'Angry IP Scanner':
     source   => "http://github.com/angryziber/ipscan/releases/download/3.2.3/ipscan-mac-3.2.3.zip",
-    provider => compressed_app,
+    provider => 'compressed_app',
   }
   
   package { 'atMonitor':
     source   => "http://download.atpurpose.com/atMonitor/atMonitor.zip",
-    provider => compressed_app,
+    provider => 'compressed_app',
   }
   
   package { 'batchmod':
     source   => "http://www.lagentesoft.com/resources/batchmod_163.zip",
-    provider => compressed_app,
+    provider => 'compressed_app',
   }
   
   package {'Carbon Copy Cloner':
-	provider => 'pkgdmg',
-	source   => 'http://www.bombich.com/software/download_ccc_update.php?v=3.4.7',
+	source   => '/Users/${::luser}/Dropbox/Config/Software/CarbonCopyCloner/ccc-3.5.4.zip',
+	provider => 'compressed_app',
   }
   
   package { 'lingonx':
     source   => "http://www.peterborgapps.com/downloads/LingonX.zip",
-    provider => compressed_app,
+    provider => 'compressed_app',
   }
   
   package { 'MunkiAdmin':
-      provider => 'appdmg',
-      source   => 'https://github.com/hjuutilainen/munkiadmin/releases/download/v0.3.0/MunkiAdmin-0.3.0.dmg',
+    source   => 'https://github.com/hjuutilainen/munkiadmin/releases/download/v0.3.0/MunkiAdmin-0.3.0.dmg',
+	provider => 'appdmg',
   }
   
   package { 'platypus':
     source   => "http://sveinbjorn.org/files/software/platypus.zip",
-    provider => compressed_app,
+    provider => 'compressed_app',
   }
 
   package { 'Remote Desktop':
   	source	 => "/Users/${::luser}/Dropbox/Config/Software/RemoteDesktop/RemoteDesktop.dmg",
-  	provider => pkgdmg,
+  	provider => 'appdmg',
   }
   
   package {'Textwrangler':
-	provider => 'appdmg',
 	source   => 'http://pine.barebones.com/files/TextWrangler_4.5.8.dmg',
+	provider => 'appdmg',
   }
   
   package { 'Transmit':
 	source      => 'http://www.panic.com/transmit/d/Transmit%204.3.2.zip',
-	provider    => compressed_app,
+	provider    => 'compressed_app',
   }
     
 }
