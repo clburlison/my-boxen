d8745ef3f62b2c7c8045f2a1224a59aa20a3b36f
Clayton Burlison <clburlison@gmail.com>
2014-04-10 19:20:02 -0500

    change shortcuts to correct path

--- modules/people/manifests/clburlison.pp
+++ modules/people/manifests/clburlison.pp
@@ -1,7 +1,7 @@
 class people::clburlison (
-  $my_username  = $people::glarizza::params::my_username,
-  $my_homedir   = $people::glarizza::params::my_homedir,
-  $my_sourcedir = $people::glarizza::params::my_sourcedir,
+  $my_username  = $people::clburlison::params::my_username,
+  $my_homedir   = $people::clburlison::params::my_homedir,
+  $my_sourcedir = $people::clburlison::params::my_sourcedir,
 ) inherits people::glarizza::params {
   ## Declare all subclasses
   include people::clburlison::applications
