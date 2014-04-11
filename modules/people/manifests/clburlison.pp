f2dadb81dd77782cdc2212dd7fb1e86c1a80493e
Clayton Burlison <clburlison@gmail.com>
2014-04-11 13:36:31 -0500

    include params.pp

--- modules/people/manifests/clburlison.pp
+++ modules/people/manifests/clburlison.pp
@@ -1,4 +1,5 @@
 class people::clburlison{
-  include people::clburlison::applications
-  include people::clburlison::config
+    include people::clburlison::applications
+    include people::clburlison::params
+    include people::clburlison::config
 }
