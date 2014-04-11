43237d81803ef07f149d41732bffbc25dd09ec2f
Clayton Burlison <clburlison@gmail.com>
2014-04-10 20:32:03 -0500

    still bad line left from a copy/paste

--- modules/people/manifests/clburlison.pp
+++ modules/people/manifests/clburlison.pp
@@ -2,7 +2,7 @@ class people::clburlison (
   $my_username  = $people::clburlison::params::my_username,
   $my_homedir   = $people::clburlison::params::my_homedir,
   $my_sourcedir = $people::clburlison::params::my_sourcedir,
-) inherits people::glarizza::params {
+) inherits people::clburlison::params {
   ## Declare all subclasses
   include people::clburlison::applications
 }
