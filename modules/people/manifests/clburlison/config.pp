34fe5444467d24c405812d68f4ff10c648573963
Clayton Burlison <clburlison@gmail.com>
2014-04-12 23:50:15 -0500

    initial: add user config settings

--- modules/people/manifests/clburlison/config.pp
+++ modules/people/manifests/clburlison/config.pp
@@ -1,5 +1,8 @@
-class people::clburlison::config {
-
+class people::clburlison::config (
+	$my_homedir   = $::people::clburlison::params::my_homedir,
+  	$my_sourcedir = $::people::clburlison::params::my_sourcedir,
+  	$my_username  = $::people::clburlison::params::my_username
+) {
   File {
     owner => $my_username,
     group => 'staff',
@@ -9,16 +12,38 @@ class people::clburlison::config {
   ###############
   # User Config #
   ###############
+  # Shows ext hd, hd, mounted servers, & removal media on desktop
+  include osx::finder::show_all_on_desktop
+  include osx::finder::unhide_library
+  include osx::finder::enable_quicklook_text_selection
+  include osx::universal_access::ctrl_mod_zoom # enables zoom by scrolling while holding Control
+  include osx::no_network_dsstores # disable creation of .DS_Store files on network shares
 
+  # Stop Preview re-opening documents
+  boxen::osx_defaults { 'Stop Preview re-opening documents':
+	  ensure => present,
+	  domain => 'com.apple.Preview',
+	  key    => 'NSQuitAlwaysKeepsWindows',
+	  value  => 'NO',
+  }
+  
+  # TextMate
+  repository { 'puppet-textmate-bundle':
+  	  source  => 'puppet-textmate-bundle/puppet-textmate-bundle',
+	  path    => "${my_sourcedir}/git/puppet-textmate-bundle",
+	  require => File["${my_sourcedir}/git"],
+  }
 
   ###################
   # Config Settings #
   ###################
-
+  include osx::global::expand_save_dialog
+  
   if ($::hostname == "011-adm-maccb") or ($::hostname == "boxen"){
 	  $recovery_message = "Property of Birdville ISD, please contact Clayton Burlison if found. clayton.burlison@birdvilleschools.net or 817-547-3819."
 	  osx::recovery_message { $recovery_message: }
   }
+  
   
   ###########################
   # License VMWare Fusion 6 #
