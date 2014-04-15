524ad9942696f1d2546b3f14bb0d16bee67ab87e
Clayton Burlison <clburlison@gmail.com>
2014-04-14 23:19:52 -0500

    add aamporter repo.

---
+++ modules/projects/manifests/aamporter.pp
@@ -0,0 +1,10 @@
+class projects::aamporter (
+	$my_homedir   = $people::clburlison::params::my_homedir,
+  	$my_sourcedir = $people::clburlison::params::my_sourcedir,
+  	$my_username  = $people::clburlison::params::my_username
+	){
+	boxen::project { 'aamporter':
+		dir		=>	"${my_sourcedir}/others/aamporter",
+		source	=>	'clburlison/aamporter',
+	}
+}
