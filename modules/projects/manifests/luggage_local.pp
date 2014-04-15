704415a55df58029d61c7ba98b591286b5be6e45
Clayton Burlison <clburlison@gmail.com>
2014-04-15 00:02:10 -0500

    upload new vendor files, and fix my comma issue on Puppetfile.

--- modules/projects/manifests/luggage_local.pp
+++ modules/projects/manifests/luggage_local.pp
@@ -1,11 +1,11 @@
 class projects::luggage_local (
-	$my_homedir   = $people::grahamgilbert::params::my_homedir,
-  	$my_sourcedir = $people::grahamgilbert::params::my_sourcedir,
-  	$my_username  = $people::grahamgilbert::params::my_username
+	$my_homedir   = $people::clburlison::params::my_homedir,
+  	$my_sourcedir = $people::clburlison::params::my_sourcedir,
+  	$my_username  = $people::clburlison::params::my_username
 	){
 
 	boxen::project { 'luggage_local':
 		dir		=>	"${my_sourcedir}/others/luggage_local",
-		source	=>	'grahamgilbert/luggage_local',
+		source	=>	'clburlison/luggage_local',
 	}
 }
