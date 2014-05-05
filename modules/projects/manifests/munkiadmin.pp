ee63affaeceeb029aab6c7047b9f9772cc6caa24
Clayton Burlison <clburlison@gmail.com>
2014-05-05 08:48:12 -0500

    remove munki-enroll add munkiadmin to others

--- modules/projects/manifests/munkiadmin.pp
+++ modules/projects/manifests/munkiadmin.pp
@@ -1,11 +1,11 @@
-class projects::munki-enroll (
+class projects::munkiadmin (
 	$my_homedir   = $people::clburlison::params::my_homedir,
   	$my_sourcedir = $people::clburlison::params::my_sourcedir,
   	$my_username  = $people::clburlison::params::my_username
 	){
 
-	# boxen::project { 'munki-enroll':
-	# 	dir		=>	"${my_sourcedir}/others/munki-enroll",
-	# 	source	=>	'edingc/munki-enroll',
-	# }
+	boxen::project { 'munkiadmin':
+		dir		=>	"${my_sourcedir}/others/munkiadmin",
+		source	=>	'hjuutilainen/munkiadmin',
+	}
 }
